249938b20859a5ea37dbc53df2e97d7a465c3c5d
dashath0097 <dashathk0440@gmail.com>
2025-02-20 16:19:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_iam_policy" "example" {
-  name        = "testpromt3"
+  name        = "example-policy"
   description = "Example policy"
 
   policy = jsonencode({
@@ -12,6 +12,11 @@ resource "aws_iam_policy" "example" {
       {
         Effect = "Allow"
         Action = "s3:ListBuckets"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = "s3:GetObject"
         Resource = "*"
       }
     ]
