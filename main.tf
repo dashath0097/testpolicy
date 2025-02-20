6e41a442b22e72e73c1ecf7099af572bbb343ff6
dashath0097 <dashathk0440@gmail.com>
2025-02-20 16:13:10 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_policy" "example" {
+  name        = "testpromt3"
+  description = "Example policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "s3:ListBuckets"
+        Resource = "*"
+      }
+    ]
+  })
+}
