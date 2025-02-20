8008a08ca84ac2897d87373dd2f8d40efc154162
dashath0097 <dashathk0440@gmail.com>
2025-02-20 16:46:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,7 @@ resource "aws_iam_policy" "example" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
-        Effect = "Allow"
-        Action = "s3:ListBuckets"
-        Resource = "*"
-      },
+    
       {
         Effect = "Allow"
         Action = "s3:GetObject"
